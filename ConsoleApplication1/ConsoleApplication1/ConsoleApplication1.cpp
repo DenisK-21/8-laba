5ab53f7805ccb661b35611f4e798fa9b48904722
User <User@LAPTOP-EGAFDKG9>
2020-06-26 01:19:40 +0300

    VisualStudio

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,44 +1,23 @@
 ﻿#include <iostream>
 #include<thread>
-#include<chrono>
-#include <ctime>
 #include<vector>
 #include<mutex>
 std::mutex mtx;
-void Odd_even_sort(int* array, int* array_min, const int i_min, const int a, const int b)
+void Min_number(int* array, int* array_min, const int i_min, const int a, const int b)
 {
-
-	bool sort_flag = false;
-	while (!sort_flag)
-	{
-		sort_flag = true;
-
-		for (int i = a + 1; i <= b - 2; i = i + 2)
-		{
-			if (array[i] > array[i + 1])
-			{
-				std::swap(array[i], array[i + 1]);
-				sort_flag = false;
-			}
-		}
-
-		for (int i = a; i <= b - 2; i = i + 2)
-		{
-			if (array[i] > array[i + 1])
-			{
-				std::swap(array[i], array[i + 1]);
-				sort_flag = false;
-			}
-		}
-
-	}
 	array_min[i_min] = array[a];
+	for (size_t i = a; i < b; i++)
+	{
+		if (array_min[i_min] > array[i])
+		{
+			array_min[i_min] = array[i];
+		}
+	}
 	mtx.lock();
 	std::cout << "№ Thread  " << i_min << " and id " << std::this_thread::get_id() << std::endl;
 	std::cout << "min number  " << array_min[i_min] << std::endl;
 	std::cout << std::endl;
 	mtx.unlock();
-
 }
 
 void Print(int* array, int& numder_array)
@@ -81,41 +60,25 @@ int main()
 	int* array_min = new int[numder_threads];
 	std::vector <std::thread> myThreads(numder_threads); //// Кол-во потоков
 	int elements_in_one_thread = 0; // узнать сколько элементов будет обрабатываться в одном потоке первоначально, не считая последнего, если ост не равен 0
-	if (numder_array % numder_threads != 0)
-	{
-		elements_in_one_thread = numder_array / numder_threads + 1;
-	}
-	else
-	{
-		elements_in_one_thread = numder_array / numder_threads;
-	}
+
+	elements_in_one_thread = numder_array / numder_threads;
+
 
 	for (size_t i = 0; i < numder_threads; i++)
 	{
 		if (i == numder_threads - 1)
-			myThreads[i] = std::thread(Odd_even_sort, std::ref(array), std::ref(array_min), i, i * elements_in_one_thread, numder_array);
+			myThreads[i] = std::thread(Min_number, std::ref(array), std::ref(array_min), i, i * elements_in_one_thread, numder_array);
 		else
-			myThreads[i] = std::thread(Odd_even_sort, std::ref(array), std::ref(array_min), i, i * elements_in_one_thread, elements_in_one_thread * (i + 1));
+			myThreads[i] = std::thread(Min_number, std::ref(array), std::ref(array_min), i, i * elements_in_one_thread, elements_in_one_thread * (i + 1));
 	}
 
 
 	for (size_t i = 0; i < numder_threads; i++)
 	{
 		myThreads[i].join();
-
 	}
-
-	//Print(array, numder_array);
-	for (int i = 0; i < numder_threads; i++)
-	{
-		for (int j = 0; j < numder_threads - 1; j++)
-		{
-			if (array_min[j] > array_min[j + 1])
-			{
-				std::swap(array_min[j], array_min[j + 1]);
-			}
-		}
-	}
+	std::cout << "     Main thread   " << std::endl;
+	Min_number(array_min, array_min, 0, 0, numder_threads);
 
 	std::cout << "the minimum number of this array  " << array_min[0] << std::endl;
 }
